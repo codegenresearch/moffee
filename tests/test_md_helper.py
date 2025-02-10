c85621434bb2cdc33c08aa45164a694906961273
venkatkrishna <venkatkrishna@moe>
2025-02-10 09:22:08 +0100

    Reset to c0b9eb12fcd82d306ff35ca60ea4d12ef392f676 and made changes for LLM Round 1

--- tests/test_md_helper.py
+++ tests/test_md_helper.py
@@ -78,16 +78,6 @@ def test_contains_deco():
     assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8)") is True
     assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9)") is True
     assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13, key14=value14)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13, key14=value14, key15=value15)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13, key14=value14, key15=value15, key16=value16)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13, key14=value14, key15=value15, key16=value16, key17=value17)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13, key14=value14, key15=value15, key16=value16, key17=value17, key18=value18)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13, key14=value14, key15=value15, key16=value16, key17=value17, key18=value18, key19=value19)") is True
-    assert contains_deco("@(key1=value1, key2=value2, key3=value3, key4=value4, key5=value5, key6=value6, key7=value7, key8=value8, key9=value9, key10=value10, key11=value11, key12=value12, key13=value13, key14=value14, key15=value15, key16=value16, key17=value17, key18=value18, key19=value19, key20=value20)") is True
 
 
 def test_extract_title():
