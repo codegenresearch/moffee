ab736c5d60b0af4784816de30a6ecd0ec8d187eb
venkatkrishna <venkatkrishna@moe>
2025-02-10 09:58:29 +0100

    Reset to c0b9eb12fcd82d306ff35ca60ea4d12ef392f676 and made changes for LLM Round 1

--- tests/test_paginate.py
+++ tests/test_paginate.py
@@ -333,6 +333,7 @@ Content 4
 ---
 Content 5
 
+
 """
     pages = composite(doc)
     assert len(pages) == 1
@@ -352,6 +353,7 @@ Content 4
 Content 5
 ---
 Content 6
+
 
 """
     pages = composite(doc)
